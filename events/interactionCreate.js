e5ca3313fcb930b78f03700d595228746637d75e
arrijalifal <ifalgantengifal@gmail.com>
2022-12-05 08:34:49 +0700

    ngerapiin dikit

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,8 +5,6 @@ const { Deta } = require('deta');
 const deta = Deta(process.env.DETA_PROJECT_KEY);
 const db = deta.Base('BotKesma');
 
-
-
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
