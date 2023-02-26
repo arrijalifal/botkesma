05f4f395c3837263fcb194a275a68d9cfeb196b9
arrijalifal <ifalgantengifal@gmail.com>
2023-02-26 23:00:21 +0700

    feedback berhasil berfungsi

--- custom_id/feedbackBot.js
+++ custom_id/feedbackBot.js
@@ -1,8 +1,9 @@
-
+const detabase = require("../detabase/detabase.js");
 module.exports = {
     data:'feedbackBot',
     async execute(interaction) {
         const feedbackUser = interaction.fields.getTextInputValue('user_feedback');
+        await detabase.feedbackCollector(feedbackUser, interaction.member.user.id);
         console.log(feedbackUser);
         await interaction.reply({content: "Terima kasih telah memberikan masukan kepada kami. Semoga Bot Kesma ini lebih berkembang lagi.", ephemeral: true})
     }
--- detabase/detabase.js
+++ detabase/detabase.js
@@ -50,6 +50,22 @@ async function accessTime(user_id) {
     }
 }
 
+async function feedbackCollector(feedbackData, user_id) {
+    const data = {
+        user_id: user_id,
+        feedback: feedbackData,
+    };
+    if (!await ac.get('feedback')) {
+        await ac.put({feedbacks: [data]}, 'feedback');
+    }
+    else {
+        let update = {
+            feedbacks: ac.util.append(data),
+        }
+        await ac.update(update, 'feedback');
+    }
+}
+
 module.exports = {
-    isRegistered, saveName, accessTime
+    isRegistered, saveName, accessTime, feedbackCollector,
 }
